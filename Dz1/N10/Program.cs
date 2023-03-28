eb1a4d8df30f7601d4529a6d29921c499968b928
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-28 12:58:07 +0300

    update N10

--- Dz1/N10/Program.cs
+++ Dz1/N10/Program.cs
@@ -4,10 +4,31 @@
 //782 -> 8
 //918 -> 1
 
+//Console.Clear();
+
+//Console.WriteLine(" Enter a number :");
+//int num = Convert.ToInt32(Console.ReadLine());
+
+//int numSer = (num/10)%10;
+//Console.WriteLine(numSer);
+
 Console.Clear();
 
-Console.WriteLine(" Enter a number :");
-int num = Convert.ToInt32(Console.ReadLine());
+int Prompt( string message)
+{
+  Console.Write(message);
+  string value = Console.ReadLine();
+  int result = Convert.ToInt32(value);
+  return result;
+}
 
-int numSer = (num/10)%10;
-Console.WriteLine(numSer);
+int number = Prompt(" Enter a a three - digit number :");
+if (number < 100 || number >= 1000)
+{
+  Console.WriteLine("Not a a three - digit number, please repeat entering");
+  return;
+}
+
+Console.WriteLine($" Введенное число {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Second number {secondRank}");
---
+++ Dz1/N13/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+Console.Clear();
+
+Console.WriteLine("Enter a three - digit number :");
+int num  = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+
