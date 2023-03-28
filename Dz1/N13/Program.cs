6a8d1ff9cf532faaaa0bb07e3e4a2b635c1b2abf
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-03-28 13:17:03 +0300

    add N13A

--- Dz1/N13/Program.cs
+++ Dz1/N13/Program.cs
@@ -6,8 +6,38 @@
 
 Console.Clear();
 
-Console.WriteLine("Enter a three - digit number :");
-int num  = Convert.ToInt32(Console.ReadLine());
+int Prompt(string message)
+{
+  Console.Write(message);
+  string value = Console.ReadLine();
+  int result = Convert.ToInt32(value);
+  return result;
+}
+
+int GetThirdRank(int number)
+{
+  while (number > 999)
+  {
+    number /= 10;
+  }
+  return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+  if (number < 100)
+  {
+    Console.WriteLine("No Third Rank");
+    return false;
+  }
+  return true;
+}
+
+int number = Prompt(" Enter a number > ");
+if (ValidateNumber(number))
+{
+  Console.WriteLine(GetThirdRank);
+}
 
 
 
