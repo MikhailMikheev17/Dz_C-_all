a3e1ca987d34967ace0d33a067baebb380989129
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-05 13:20:17 +0300

    add N27

---
+++ Dz1/N27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+//452 -> 11
+
+//82 -> 10
+
+//9012 -> 12
+
+Console.Clear();
+
+int SetNumber(string text = "")
+{
+  System.Console.Write($"Enter a numer{text} : ");
+  int num = int.Parse(Console.ReadLine());
+  return num;
+}
+
+int Summa(int number)
+{
+  int result = 0;
+  while (number > 0)
+  {
+    result += number % 10;
+    number = number / 10;
+  }
+  return result;
+}
+
+int number = SetNumber();
+System.Console.WriteLine($"summ all digit = {Summa(number)}");
