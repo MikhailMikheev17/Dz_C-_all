5b74711715b1020ab1547dbf9dabfa5fc581173c
MikhailMikheev <Mikhail-Mikheev-7@yandex.ru>
2023-04-11 13:53:57 +0300

    add N41

---
+++ Dz1/N41/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+
+1, -7, 567, 89, 223-> 3*/
+
+Console.Clear();
+
+System.Console.WriteLine(" enter numbers (разделитель пробел) >  ");
+int[] array = Console.ReadLine()!.Split(' ').Select(x => int.Parse(x)).ToArray();
+
+int count = CheckCountPos(array);
+System.Console.WriteLine($" count = {count}");
+
+int CheckCountPos(int[] array)
+{
+  int count = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if(array[i] > 0) count+= 1;
+  }
+  return count;
+}
+
